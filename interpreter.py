0fffde12d21a1e37ae31a6ad5f6da0b9917f20e5
vicktor111 <85438133+vicktor111@users.noreply.github.com>
2021-06-09 11:29:16 +0300

    code

--- interpreter.py
+++ interpreter.py
@@ -1,7 +1,7 @@
 from tkinter import Tk, Button, Label
 class name_file: ...
-
-def interpreter(file: name_file)->print:
+class Print: ...
+def interpreter(file: name_file)->Print:
     l=[a for a in file.split("\n")]
     print(l)
     lab=0
@@ -9,7 +9,7 @@ def interpreter(file: name_file)->print:
     f=0
     g=0
     for i in l:
-        if i[0:len(l)+1]=="start;": #1
+        if i[0:6]=="start;": #1
             window=Tk()
         if i[0:9]=="geometry:":
             i=i[10:len(i)+1]
@@ -20,24 +20,24 @@ def interpreter(file: name_file)->print:
             c=[i for i in i.split(",")]
             window.title(f"{c[0][0:len(c[0])-1]}")
 
-        if i[0:len(l)+1]=="end;": #2
+        if i[0:4]=="end;": #2
             window.mainloop()
 
-        if i[0:len(l)+1]=="Button;": #3
+        if i[0:7]=="Button;": #3
             button=Button(window)
             button.pack()
 
-        if i[0:len(l)+1]=="Label;": #3
+        if i[0:6]=="Label;": #3
             label=Label(window, text="Text")
             label.pack()
 
-        if i[0:len(l)+1]=="Button{": #4
+        if i[0:7]=="Button{": #4
             button=Button(window)
             g=0
             f=1
             but=1
 
-        if i[0:len(l)+1]=="Label{": #4
+        if i[0:6]=="Label{": #4
             label=Label(window)
             g=0
             f=1
@@ -79,7 +79,7 @@ def interpreter(file: name_file)->print:
             f=1
             g=1
 
-        if i=="}": #9
+        if i[0]=="}": #9
             if but==1:
                 but=0
                 if g==1:
@@ -112,4 +112,7 @@ end;
 """
 
 if __name__=="__main__":
-    interpreter(f)
+    filename=input("")
+    file=open(filename,"r",encoding="utf-8")
+    file=file.read()
+    interpreter(file)
