fb80864d67d578686efb771bcf3b014f4c0bfdd4
vicktor111 <85438133+vicktor111@users.noreply.github.com>
2021-06-10 11:58:01 +0300

    Добавил нові віджети.

--- interpreter.py
+++ interpreter.py
@@ -1,20 +1,36 @@
-from tkinter import Tk, Button, Label
+from tkinter import Tk, Button, Label, Entry, Text
 class name_file: ...
 class Print: ...
 
 def interpreter(file: name_file)->Print:
 
-    def atribute_bg(wiget,index):
-            wiget.config(bg=f"{code[index:len(code)+1]}")
+    def atribute_bg(wiget,index,code):
+        if code.count("#")==1:
+            code=[s for s in code.split("#")]
+            wiget.config(bg=f"{code[0][index:len(code[0])-1]}")
+        else:
+            wiget.config(bg=f"{code[index:len(code)]}")
     
-    def atribute_width(wiget,index):
-            wiget.config(width=f"{code[index:len(code)+1]}")
+    def atribute_width(wiget,index,code):
+        if code.count("#")==1:
+            code=[s for s in code.split("#")]
+            wiget.config(width=f"{code[0][index:len(code[0])-1]}")
+        else:
+            wiget.config(width=f"{code[index:len(code)]}")
 
-    def atribute_height(wiget,index):
-            wiget.config(height=f"{code[index:len(code)+1]}")
+    def atribute_height(wiget,index,code):
+        if code.count("#")==1:
+            code=[s for s in code.split("#")]
+            wiget.config(height=f"{code[0][index:len(code[0])-1]}")
+        else:
+            wiget.config(height=f"{code[index:len(code)]}")
 
-    def atribute_text(wiget,index):
-            wiget.config(text=f"{code[index:len(code)+1]}")
+    def atribute_text(wiget,index,code):
+        if code.count("#")==1:
+            code=[s for s in code.split("#")]
+            wiget.config(text=f"{code[0][index:len(code[0])-1]}")
+        else:
+            wiget.config(text=f"{code[index:len(code)]}")
 
     def locage(wiget,value):
         if value==1:
@@ -26,9 +42,11 @@ def interpreter(file: name_file)->Print:
     print(list_with_code)
     label_1=0
     button_1=0
-    f=0
-    g=0
-    n=0
+    entry_1=0
+    text_1=0
+    # f=0
+    # g=0
+    # n=0
     for code in list_with_code:
         if code[0:6]=="start;": #1.
             window=Tk()
@@ -62,31 +80,56 @@ def interpreter(file: name_file)->Print:
             f=1
             label_1=1
 
+        elif code[0:6]=="Entry{": #4.
+            entry=Entry(window)
+            g=0
+            f=1
+            entry_1=1
+
+        elif code[0:5]=="Text{": #4.
+            text=Text(window)
+            g=0
+            f=1
+            text_1=1
+
         elif code[0:5]=="  bg:" and f==1: #5.
             if button_1==1:
-                atribute_bg(button,6)
+                atribute_bg(button,6,code)
             if label_1==1:
-                atribute_bg(label,6)
+                atribute_bg(label,6,code)
+            if entry_1==1:
+                atribute_bg(entry,6,code)
+            if text_1==1:
+                atribute_bg(text,6,code)
             f=1
 
         elif code[0:6]=="  wid:" and f==1: #6.
             if button_1==1:
-                atribute_width(button,7)
+                atribute_width(button,7,code)
             if label_1==1:
-                atribute_width(label,7)
+                atribute_width(label,7,code)
+            if entry_1==1:
+                atribute_width(entry,7,code)
+            if text_1==1:
+                atribute_width(text,7,code)
+            f=1
 
         elif code[0:7]=="  heig:" and f==1: #7.
             if button_1==1:
-                atribute_height(button,8)
+                atribute_height(button,8,code)
             if label_1==1:
-                atribute_height(label,8)
+                atribute_height(label,8,code)
+            if entry_1==1:
+                atribute_height(entry,8,code)
+            if text_1==1:
+                atribute_height(text,8,code)
             f=1
 
         elif code[0:7]=="  text:" and f==1: #8.
             if button_1==1:
-                atribute_text(button,8)
+                atribute_text(button,8,code)
             if label_1==1:
-                atribute_text(label,8)
+                atribute_text(label,8,code)
             f=1
 
         elif code[0:8]=="  place:" and f==1: #9.
@@ -100,14 +143,17 @@ def interpreter(file: name_file)->Print:
         elif code=="}": #10.
             if button_1==1:
                 locage(button,g)
-                button_1=0    
+                button_1=0
             if label_1==1:
                 locage(label,g)
                 label_1=0
+            if entry_1==1:
+                locage(entry,g)
+                entry_1=0
             f=0
-        elif code=="":
+        elif code=="": #11.
             pass
-        elif code[0]=="#":
+        elif code[0]=="#": #12.
             pass
         else:
             raise SyntaxError(f"{code}")
