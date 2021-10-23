040825d5f9dfa68fa2614818199110d40c5bc77a
vicktor111 <85438133+vicktor111@users.noreply.github.com>
2021-10-23 12:31:45 +0300

    Змінив назву файла Life.py на main.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from tkinter import Tk, Button, Label, Entry, Frame, END
-import Life
+import main
 
 string = """#863200
 #c8c8c8
@@ -12,7 +12,7 @@ class Config:
         frame = Frame(master, width=len(text) + width)
         Label(frame, text=text).grid(row=0, column=0)
         self._text = Entry(frame, width=width)
-        self._text.insert(0, Life.open_file()[num])
+        self._text.insert(0, main.open_file()[num])
         self._text.grid(row=0, column=1)
         frame.place(x=x, y=y - 2)
 
@@ -36,16 +36,17 @@ def set_info(info, list_with_configs):
 
 def config(window, function):
     root = Tk()
-    root.geometry("200x200")
+    root.geometry("300x200")
     root.title("Настройки")
+    # root.resizable(False, False)
     Label(root, text="Настройки").pack()
     con_1 = Config(root, "Задній фон", 10, 20, 0, 10)
     con_2 = Config(root, "Колір кнопки", 10, 40, 1, 10)
     con_3 = Config(root, "Колір кнопки після нажимання", 10, 60, 2, 10)
-    btn = Button(root, text="save",
+    btn = Button(root, text="зберегти",
                  command=lambda: save_info(f"{con_1.getinfo()}\n{con_2.getinfo()}\n{con_3.getinfo()}", function))
-    btn_2= Button(root, text="1",
-                    command=lambda:set_info(string.split("\n"),[con_1, con_2, con_3]))
+    btn_2 = Button(root, text="1",
+                   command=lambda: set_info(string.split("\n"), [con_1, con_2, con_3]))
     btn.place(x=30, y=90)
-    btn_2.place(x=90,y=90)
+    btn_2.place(x=110, y=90)
     root.mainloop()
